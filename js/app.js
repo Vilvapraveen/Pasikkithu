ae01ca5ab53a9baeaefaacde2c27e3d9fec32398
VILVA PRAVEEN <113287924+Vilvapraveen@users.noreply.github.com>
2026-04-10 00:03:42 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,16 +22,3 @@ function addToCart(id) {
 }
 
 loadProducts();
-
-
-
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-  const product = products.find(p => p.id === id);
-  cart.push(product);
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  alert("Added to cart");
-}
